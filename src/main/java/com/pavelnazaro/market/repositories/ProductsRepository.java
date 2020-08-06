9d0ee3c85dcc342efee96926e9f83717fc095351
pavelnazaro <pavelnazaro@mail.ru>
2020-08-07 01:50:42 +0300

    Lesson7 files

--- src/main/java/com/pavelnazaro/market/repositories/ProductsRepository.java
+++ src/main/java/com/pavelnazaro/market/repositories/ProductsRepository.java
@@ -1,10 +1,14 @@
 package com.pavelnazaro.market.repositories;
 
 import com.pavelnazaro.market.entities.Product;
+import com.pavelnazaro.market.entities.dtos.ProductDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    List<ProductDto> findAllBy();
 }
